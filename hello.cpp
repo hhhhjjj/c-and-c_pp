a35a935d036c185b4e5417930c60d4f7d2aa2673
HeJames <48619125+hhhhjjj@users.noreply.github.com>
2019-08-07 17:38:05 +0800

    Initial commit

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main(){
+//	std::cout<<"hello";
+//	cout是console输出的意思
+//	std::cin.get();
+//  输入就是cin 
+
+	float price = 1.5;
+//	在一行进行变量类型定义和赋值 
+//c++比c要容易输出变量的值，用cout就行
+	std::cout<<"has "<<price<<" mile"; 
+//	当然这个price也可以 在这进行一些计算 
+//什么四舍五入、格式化输出这些用的时候再百度吧 
+	std::cout<<"-"<<"1"<<"-";
+//	这个设置输出宽度这有问题 
+	setw().
+	setw(5);
+	std::cout<<"-"<<"1"<<"-";
+	return 0;
+}
